c1214dd61f1836e8537f176517bf42186346ab31
ferney quiceno <104113942+Jhonquicen@users.noreply.github.com>
2022-09-28 14:49:26 -0500

    PrimerReactJsx

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+class App extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Hola Dojo</h1>
+        <h3>Cosas que necesito hacer:</h3>
+        
+        <ul>
+            <li> Estudiar</li>
+            <li> Montar en globo</li>
+            <li> Conocer otro pais</li>
+            <li> ir al tomorrowland</li>
+        </ul>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
